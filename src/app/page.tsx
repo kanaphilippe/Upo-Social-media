8a6a96ac9d55c18bdad6282dc457508207dea04c
philippe <dekanaphilippe@yahoo.com>
2025-07-08 00:42:11 +0200

    ok its good

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
+"use client";
+import Image from "next/image"
+import { IKImage } from "imagekitio-next";
+
+const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
+
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
+    <div className=''>
+      <IKImage urlEndpoint={urlEndpoint}
+        path="general/post.jpeg"
+        width={600}
+        height={600}
+        alt="Post image"
+      />
+    </div>
   )
 }
 
